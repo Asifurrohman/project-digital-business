fe493223c2e9ee2956d343b5dce0ecfc26f58d6e
Asifurrohman <akun.asifurrohman@gmail.com>
2026-04-10 10:08:04 +0700

    tugas 2: styling halaman contact

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,23 @@ Route::get('/', function () {
     return '<h1>25.12.3722 - Muhammad Asifurrohman</h1>';
 });
 
+Route::get('/about', function () {
+    return '<h1>Halaman About EventHub</h1>';
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/catalog', function () {
+    return view('catalog');
+});
+
+Route::get('/bantuan', function () {
+    return view('bantuan');
+});
+
+Route::get('/profile', function () {
+    return view('profil');
+});
+
